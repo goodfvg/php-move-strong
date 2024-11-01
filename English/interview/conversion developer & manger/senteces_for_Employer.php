22e04f0fe272b2ad155362bb6907bb5aee9cdf33
progood <tfalsoh@gmail.com>
2024-10-31 19:43:12 -0700

    my first commit

---
+++ English/interview/conversion developer & manger/senteces_for_Employer.php
@@ -0,0 +1,13 @@
+I understand that moving can be expensive. What are you proposing
+
+I understand that moving can be expensive. What do you suggest
+------------------------------------------------------------------------
+Hi Murad, sure, what do you have in mind?
+
+Hello murad , sure, what's on your mind?
+------------------------------------------------------------------------
+That's an interesting proposal. Can you provide me with more details about the amount you're requesting and how you plan to pay it back?
+
+This is an interesting suggestion. Can you provide me with more details about the amount you are requesting and how you plan to pay it?
+------------------------------------------------------------------------
+
